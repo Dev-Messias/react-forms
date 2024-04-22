8bceb60d768f29e36f76c843ff970c57d5684ba6
DevMessias <messiasbento944@gmail.com>
2024-04-22 18:29:53 -0300

    Identificando renderizações.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,80 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Header } from './components/Header';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [username, setUsername] = useState("")
+  const [description, setDescription] = useState("")
+  const [type, setType] = useState("user")
+
+  function handleSave(e){
+    e.preventDefault();
+
+    console.log({
+      name,
+      email,
+      username,
+      description,
+      type,
+    })
+  }
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+      <h1>React</h1>
+      <Header/>
+
+      <form className="form" onSubmit={handleSave}>
+        <input
+          type="text"
+          value={name}
+          onChange={ (event) => setName(event.target.value) }
+          placeholder="Digite seu nome..."
+          className="input"
+        />
+
+        <input
+          type="text"
+          value={email}
+          onChange={ (event) => setEmail(event.target.value) }
+          placeholder="Digite seu email..."
+          className="input"
+        />
+
+        <input
+          type="text"
+          value={username}
+          onChange={ (event) => setUsername(event.target.value) }
+          placeholder="Digite seu username..."
+          className="input"
+        />
+
+        <textarea
+          type="text"
+          value={description}
+          onChange={ (event) => setDescription(event.target.value) }
+          placeholder="Digite sua descriçao..."
+          className="input"
+        ></textarea>
+
+
+        <select  
+          className="select"
+          value={type}
+          onChange={e => setType(e.target.value)}
+        >
+          <option value="user">user</option>
+          <option value="admin">admin</option>
+        </select>
+
+
+        <button className="button" type="submit">Enviar</button>
+      </form>
+    </div>
   )
 }
 
